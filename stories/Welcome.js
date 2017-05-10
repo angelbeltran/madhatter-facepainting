b0bf56f809629eca6b770fb7a42a0bad5f0a16c6
Angel Beltran <beltranbeaverdam@gmail.com>
2017-05-10 09:58:15 -0600

    linted stories

--- stories/Welcome.js
+++ stories/Welcome.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
+
+/* eslint-disable jsx-a11y/href-no-hash*/
 
 const styles = {
   main: {
@@ -22,8 +24,8 @@ const styles = {
   code: {
     fontSize: 15,
     fontWeight: 600,
-    padding: "2px 5px",
-    border: "1px solid #eae9e9",
+    padding: '2px 5px',
+    border: '1px solid #eae9e9',
     borderRadius: 4,
     backgroundColor: '#f3f2f2',
     color: '#3a3a3a',
@@ -31,13 +33,22 @@ const styles = {
 
   note: {
     opacity: 0.5,
-  }
+  },
 };
 
 export default class Welcome extends React.Component {
+  static propTypes = {
+    showApp: PropTypes.func,
+  }
+
+  constructor() {
+    super();
+    this.showApp.bind(this);
+  }
+
   showApp(e) {
     e.preventDefault();
-    if(this.props.showApp) this.props.showApp();
+    if (this.props.showApp) this.props.showApp();
   }
 
   render() {
@@ -48,14 +59,14 @@ export default class Welcome extends React.Component {
           This is a UI component dev environment for your app.
         </p>
         <p>
-          We've added some basic stories inside the <code style={styles.code}>src/stories</code> directory.
-          <br/>
+          {"We've added some basic stories inside the <code style={styles.code}>src/stories</code> directory."}
+          <br />
           A story is a single state of one or more UI components. You can have as many stories as you want.
-          <br/>
+          <br />
           (Basically a story is like a visual test case.)
         </p>
         <p>
-          See these sample <a style={styles.link} href='#' onClick={this.showApp.bind(this)}>stories</a> for a component called <code style={styles.code}>Button</code>.
+          See these sample <a style={styles.link} href="#" onClick={this.showApp}>stories</a> for a component called <code style={styles.code}>Button</code>.
         </p>
         <p>
           Just like that, you can add your own components as stories.
@@ -67,12 +78,12 @@ export default class Welcome extends React.Component {
         </p>
         <p>
           This is just one thing you can do with Storybook.
-          <br/>
+          <br />
           Have a look at the <a style={styles.link} href="https://github.com/kadirahq/react-storybook" target="_blank">React Storybook</a> repo for more information.
         </p>
         <p style={styles.note}>
           <b>NOTE:</b>
-          <br/>
+          <br />
           Have a look at the <code style={styles.code}>.storybook/webpack.config.js</code> to add webpack
           loaders and plugins you are using in this project.
         </p>
